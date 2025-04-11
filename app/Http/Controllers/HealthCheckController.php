ff591a3cb0b61653f652da3e6fb8ba95cf1edc9d
Adrian <adrianmarrero18@gmail.com>
2025-04-11 23:54:10 +0200

    refactor(health-check) - Create log messages and add cache/database test

--- app/Http/Controllers/HealthCheckController.php
+++ app/Http/Controllers/HealthCheckController.php
@@ -3,6 +3,11 @@
 namespace App\Http\Controllers;
 
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+use Throwable;
+use Exception;
 
 class HealthCheckController extends Controller
 {
@@ -13,7 +18,42 @@ class HealthCheckController extends Controller
     {
         return response()->json([
             'status' => 'This is the fucking hell!',
+            'data' => [
+                'app'   => true,
+                'db'    => $this->checkDatabase(),
+                'cache' => $this->checkCache(),
+            ],
             'timestamp' => now()->toDateTimeString(),
         ]);
+    }
+
+    private function checkDatabase(): bool
+    {
+        try {
+            DB::connection()->getPdo();
+            return true;
+        } catch (Exception $e) {
+            Log::warning('Database connection error', [
+                'message' => $e->getMessage(),
+                'trace'   => $e->getTraceAsString(),
+            ]);
+
+            return false;
+        }
+    }
+
+    private function checkCache(): bool
+    {
+        try {
+            Cache::put('health_check', 1, 10);
+            return Cache::get('health_check') === 1;
+        } catch (Throwable $e) {
+            Log::error('Cache connection error', [
+                'message' => $e->getMessage(),
+                'trace'   => $e->getTraceAsString(),
+            ]);
+
+            return false;
+        }
     }
 }
